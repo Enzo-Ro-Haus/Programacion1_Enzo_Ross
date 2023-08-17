86100cb737c4c55888002703ef7061e1feb68878
Enzo-Ro-Haus <105392239+Enzo-Ro-Haus@users.noreply.github.com>
2023-08-16 21:02:25 -0300

    Add files via upload

---
+++ ejercicios_complementarios_Enzo_Rosso.py
@@ -0,0 +1,85 @@
+#Alumno: Enzo Rosso Hausberger
+
+#Ejercicios complementarios
+
+#1 
+numero1 = 35
+print("1) numero1:\t",numero1)
+
+#2
+numero2 = 20.5
+print("2) numero2:\t",numero2)
+
+#3
+suma = numero1 + numero2
+print("3) suma:\t", suma)
+
+#4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print("4)\nresta:\t", resta)
+print("multiplicacion:\t", multiplicacion)
+print("division:\t", division)
+
+#5
+nombre = "Enzo"
+print("5) nombre:\t",nombre)
+
+#6
+precio = 200.8
+print("6) precio:\t", precio)
+
+#7
+descuento = 0.5
+print("7) descuento\t",descuento)
+
+#8
+precio_final = precio * descuento
+print("8) precio_final:\t", precio_final)
+
+#9
+cadena = "\"Obra de tal modo que trates a la humanidad, tanto en ti como en la de otro, siempre como un fin y nunca solamente como un medio. -Kant\""
+print("9) cadena:\t",cadena)
+
+#10
+longitud = str(len(cadena))
+print("10) longitud:\t",longitud)
+
+#11
+precio = int(300)
+print("11) precio:\t",precio, "tipo: ", type(precio))
+
+#12
+nombre = "Enzo"
+apellido = "Rosso Hausberger"
+nombre_completo = nombre, " ", apellido
+print("12) nombre_completo:\t", nombre_completo)
+
+
+#13
+mi_edad = 27
+print("13)\nmi_edad:\t",mi_edad)
+mi_edad += 5
+print("mi_edad con incremento de 5:\t",mi_edad)
+mi_edad -= 10
+print("mi_edad disminuida en 10:\t",mi_edad)
+
+#14
+altura = 1.80
+print("14)\naltura:\t",altura)
+altura *= 4
+print("altura multiplicada por 4:\t",altura)
+altura /= 3
+print("altura dividida por 3:\t",altura)
+
+
+#15
+nombre = "ENZO"
+nombre = nombre.lower()
+print("15) nombre modificado:\t", nombre)
+
+#16
+nombre = nombre.capitalize()
+print("16) nombre modificado\t",nombre)
